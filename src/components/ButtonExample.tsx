516d56a2bd5a72b4bcfa8e32045aa2cea9831c6c
David Zearing <dzearing@microsoft.com>
2020-05-27 22:48:59 -0700

    trying react-button.

--- src/components/ButtonExample.tsx
+++ src/components/ButtonExample.tsx
@@ -1,33 +1,20 @@
 import * as React from "react";
-import { Button } from "./Button";
+import { Button } from "@fluentui/react-button";
 import { UploadIcon, ChevronDownIcon, CircleRingIcon } from "../icons/IconSet";
 
 export const ButtonExampleSection = (props: any) => (
   <div className="SampleCard">
     <div>{Object.keys(props).join(" ").toString() || "(default)"}</div>
-    <Button iconOnly {...props}>
-      <UploadIcon />
-    </Button>
-    <Button circular {...props}>
-      <UploadIcon />
-    </Button>
-    <Button {...props}>
-      <span>I'm a button</span>
-    </Button>
-    <Button {...props}>
-      <CircleRingIcon />
-      <span>I'm a button</span>
-    </Button>
-    <Button {...props}>
-      <CircleRingIcon />
-      <span>I'm a button</span>
-      <ChevronDownIcon style={{ fontSize: 12 }} />
-    </Button>
-    <Button disabled {...props}>
-      <CircleRingIcon />
-      <span>I'm a disabled button</span>
-      <ChevronDownIcon style={{ fontSize: 12 }} />
-    </Button>
+    <Button iconOnly icon={<UploadIcon />} {...props} />
+    <Button iconOnly icon={<UploadIcon />} circular {...props} />
+    <Button {...props} content={<span>I'm a button</span>} />
+    <Button {...props} icon={<CircleRingIcon />} content="I'm a button" />
+    <Button
+      {...props}
+      disabled
+      icon={<CircleRingIcon />}
+      content="I'm a button"
+    />
   </div>
 );
 
